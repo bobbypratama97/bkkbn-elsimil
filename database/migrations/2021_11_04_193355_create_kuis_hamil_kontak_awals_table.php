57e4eaf5f25e19fe974de5ca7d09b438723fba08
Bobby Pratama <bpratama77@gmail.com>
2021-11-04 19:58:27 +0700

    + kuishamilkontakawal model and migration

---
+++ database/migrations/2021_11_04_193355_create_kuis_hamil_kontak_awals_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKuisHamilKontakAwalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kuis_hamil_kontak_awals', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_user');
+            $table->integer('id_member');
+            $table->string('nama');
+            $table->string('nik');
+            $table->integer('usia');
+            $table->string('alamat');
+            $table->integer('jumlah_anak');
+            $table->integer('usia_anak_terakhir');
+            $table->boolean('anak_stunting');
+            $table->date('hari_pertama_haid_terakhir');
+            $table->boolean('sumber_air_bersih');
+            $table->boolean('rumah_layak_huni');
+            $table->boolean('bansos');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kuis_hamil_kontak_awals');
+    }
+}
