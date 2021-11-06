dc19e3ae715a975fc71edd2809d50577f49c3bfa
Bobby Pratama <bpratama77@gmail.com>
2021-11-06 10:24:41 +0700

    + kuisioner hamil pasca persalinan

---
+++ database/migrations/2021_11_06_101902_create_kuis_hamil_persalinans_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKuisHamilPersalinansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kuisioner_hamil_persalinan', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_user');
+            $table->integer('id_member');
+            $table->date('tanggal_persalinan');
+            $table->boolean('kb');
+            $table->integer('usia');
+            $table->integer('berat');
+            $table->integer('panjang_badan');
+            $table->integer('jumlah');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kuis_hamil_persalinans');
+    }
+}
