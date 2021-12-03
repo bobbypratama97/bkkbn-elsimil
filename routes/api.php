889c28a7cde0853cbb4c6e27046ebf9598012869
Yulius Giann <ydgiannp@gmail.com>
2021-12-03 23:47:34 +0700

    copywtriting pernyataan di API

--- routes/api.php
+++ routes/api.php
@@ -77,5 +77,5 @@ Route::group(['namespace' => 'Api\v1', 'prefix' => 'v1'], function() {
 
 	Route::post('version', 'SettingController@check_version_code');
 
-    Route::get('kuesioner-hamil/{id}','KuisHamilController@getKuesionerHamilResult');
+  Route::get('kuesioner-hamil/{id}','KuisHamilController@getKuesionerHamilResult');
 });
